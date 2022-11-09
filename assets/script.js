6dd2371e0ad65babb55a04afc30cfb75eb062257
Barrerse <sebastianbarrera@live.com>
2022-11-08 22:41:04 -0500

    added Weather and Forecast HTML cards, line 50 & started JS

--- assets/script.js
+++ assets/script.js
@@ -21,22 +21,10 @@
 
 // SEARCH HISTORY FUNCTION 
 
-var baseUrl = "https://api.openweathermap.org"
+// STORE API INFORMATION IN VARIABLES
+var baseUrl = "https://api.openweathermap.org";
+var APIkey = "d882ee08c41452ba3fd165e1c9e8e0a8";
 
+// STORE JQUERY SELECTORS IN VARIABLES
+var rsltEl = document.querySelector('#searchCity');
 
-function get5DaysForecast(apiResponse) {
-    return apiResponse.list.reduce(function(days,current) {
-        var currentDate=current.dt_txt.split(" ")[0];
-        if(!days.find(function(day) {
-            return day.dt_txt.includes(currentDate);
-        })) {
-            return [...days,current];
-        }
-        return days;
-    },[])
-}
-console.log (currentDate);
-
-var = deeznuts
-
-localStorage.setItem()
